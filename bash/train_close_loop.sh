d1aaf724ad0a7f40c11c37cd96dce27b7d7d7d95
Iven-Wu <ivenwu123@gmail.com>
2024-04-25 15:17:37 -0700

    1

--- bash/train_close_loop.sh
+++ bash/train_close_loop.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 N_FUTURE=1
+NUM_EPOCHS=24
 
 python -u src/run.py \
 trainer=womd \
+trainer.max_epochs=$NUM_EPOCHS \
 model=scg_womd \
 model.model.decoder.n_pred=6 \
 model.model.decoder.mlp_head.n_step_future=$N_FUTURE \
 model.pre_processing.scene_centric.n_step_future=$N_FUTURE \
 datamodule=h5_womd \
-datamodule.batch_size=3 \
+datamodule.batch_size=12 \
 datamodule.num_workers=8 \
-loggers.wandb.name="hptr_womd" \
+loggers.wandb.name="hptr_womd_close" \
 loggers.wandb.project="sim_agents" \
 loggers.wandb.entity="team-wu" \
-loggers.wandb.offline=True \
-datamodule.data_dir='/localhome/ywa445/local-scratch/datasets/waymo' \
-hydra.run.dir='logs/${now:%Y-%m-%d}/${now:%H-%M-%S}'
+loggers.wandb.offline=False \
+datamodule.data_dir='/home/fuyang/code/HPTR/datasets/waymo' \
+hydra.run.dir='logs/global_close/${now:%Y-%m-%d}/${now:%H-%M-%S}'
 
